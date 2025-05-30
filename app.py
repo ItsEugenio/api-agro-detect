662b59f46441f6c5ae6d2decde33406301dce7ed
ItsEugenio <223730@ids.upchiapas.edu.mx>
2025-05-30 02:31:17 -0600

    fix: change port for Railway

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ import numpy as np
 import cv2
 from agro_model import load_model, load_class_names, load_and_preprocess_image_bytes, predict
 from flask_cors import CORS
+import os
+from datetime import datetime
 
 app = Flask(__name__)
 CORS(app)
@@ -41,6 +43,14 @@ def predict_base64():
 
     except Exception as e:
         return jsonify({"error": str(e)}), 500
+    
+@app.route("/status", methods=["GET"])
+def status():
+    return jsonify({
+        "status": "API funcionando correctamente",
+        "timestamp": datetime.now().isoformat()
+    })    
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
